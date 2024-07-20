b96de6a34f9d51eef1deeb35bcab12d3c5019821
☕ Tim <tim@wants.coffee>
2024-07-20 17:41:34 +0800

    Add git scraper for Auora board locations

---
+++ scrape-locations.sh
@@ -0,0 +1,11 @@
+download_locations () {
+	curl https://api.$1.com/v1/pins?types=gym | jq . > ./data/$1-locations.json
+	echo "Downloaded ./data/$1-locations.json"
+}
+
+download_locations auroraboardapp
+download_locations decoyboardapp
+download_locations grasshopperboardapp
+download_locations kilterboardapp
+download_locations tensionboardapp2
+download_locations touchstoneboardapp
